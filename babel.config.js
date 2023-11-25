4fee948e51c3576d60a256d425a16e62cd6ee730
Suraj <48823022+SurajGupta07@users.noreply.github.com>
2023-11-25 22:38:06 +0530

    feat: added react-native-dotenv (#6)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env",
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,3 +1,4 @@
+import {API_URL} from "@env";
 import React from "react";
 import {Button, Text, View} from "react-native";
 import {THome} from "./types";
@@ -5,7 +6,9 @@ import {THome} from "./types";
 export const Home: React.FC<THome> = ({navigation}) => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>
+        Accessing env example {"->"} {API_URL}
+      </Text>
       <Button
         title="Go to details"
         onPress={() => navigation.navigate("Details")}
