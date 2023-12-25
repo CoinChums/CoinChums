f2f6fdc4dd72bf98aedd6bbb89d56212d1732618
SurajGupta07 <bgsuraj07@gmail.com>
2023-12-25 20:31:07 +0530

    fix: fixed onboarding screen shown on first launch

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,20 +1,41 @@
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import React, {FC} from "react";
-import {APP_ROUTES} from "../constants/enums";
-import {Launch} from "../screens";
-import {NavigationParams} from "../types/types";
-import {OnboardingComponent} from "../screens/Onboarding/Onboarding";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { FC, useEffect, useState } from 'react';
+import { IndicatorView } from '../components';
+import { APP_ROUTES } from '../constants/enums';
+import { Launch } from '../screens';
+import { OnboardingComponent } from '../screens/Onboarding/Onboarding';
+import { NavigationParams } from '../types/types';
 
 const Stack = createNativeStackNavigator<NavigationParams>();
 
 export const AuthStack: FC = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('appLaunched').then(value => {
+      if (value === null) {
+        AsyncStorage.setItem('appLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  if (isFirstLaunch === null) {
+    return <IndicatorView isLoading={true} />;
+  }
+
   return (
-    <Stack.Navigator screenOptions={{animation: "slide_from_left"}}>
-      <Stack.Screen name={APP_ROUTES.onboarding} component={OnboardingComponent} options={{headerShown: false}} />
+    <Stack.Navigator screenOptions={{ animation: 'slide_from_left' }}>
+      {isFirstLaunch && (
+        <Stack.Screen name={APP_ROUTES.onboarding} component={OnboardingComponent} options={{ headerShown: false }} />
+      )}
       <Stack.Screen
         name={APP_ROUTES.launch}
         component={Launch}
-        options={{headerShown: false, animation: "slide_from_right"}}
+        options={{ headerShown: false, animation: 'slide_from_right' }}
       />
     </Stack.Navigator>
   );
--- src/screens/Onboarding/Onboarding.tsx
+++ src/screens/Onboarding/Onboarding.tsx
@@ -1,16 +1,12 @@
-import {NavigationProp, useNavigation} from "@react-navigation/native";
-import {Image} from "react-native";
-import Onboarding from "react-native-onboarding-swiper";
-import {APP_ROUTES} from "../../constants/enums";
-import {NavigationParams} from "../../types/types";
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { Image } from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+import { APP_ROUTES } from '../../constants/enums';
+import { NavigationParams } from '../../types/types';
 
 export const OnboardingComponent = () => {
   const navigation = useNavigation<NavigationProp<NavigationParams>>();
-  const handleSkip = () => {
-    navigation.navigate(APP_ROUTES.launch);
-  };
-
-  const handleDone = () => {
+  const handleNext = () => {
     navigation.navigate(APP_ROUTES.launch);
   };
 
@@ -18,20 +14,20 @@ export const OnboardingComponent = () => {
     <Onboarding
       pages={[
         {
-          backgroundColor: "#fff",
-          image: <Image source={require("../../assets/images/coinchums.png")} />,
-          title: "Welcome",
-          subtitle: "This is the welcome screen",
+          backgroundColor: '#fff',
+          image: <Image source={require('../../assets/images/coinchums.png')} />,
+          title: 'Welcome',
+          subtitle: 'This is the welcome screen',
         },
         {
-          backgroundColor: "#fff",
-          title: "Introduction",
-          subtitle: "Introduce your app here",
-          image: <Image source={require("../../assets/images/coinchums.png")} />,
+          backgroundColor: '#fff',
+          title: 'Introduction',
+          subtitle: 'Introduce your app here',
+          image: <Image source={require('../../assets/images/coinchums.png')} />,
         },
       ]}
-      onSkip={handleSkip}
-      onDone={handleDone}
+      onSkip={handleNext}
+      onDone={handleNext}
       showSkip={true}
       bottomBarHighlight={false}
       nextLabel="Next"
