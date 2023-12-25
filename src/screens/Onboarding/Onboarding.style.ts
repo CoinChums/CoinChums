49d0716e4d574c23c72ad848e1e17f7998fac380
SurajGupta07 <bgsuraj07@gmail.com>
2023-12-25 23:18:56 +0530

    refactor: removed onboarding package

--- src/screens/Onboarding/Onboarding.style.ts
+++ src/screens/Onboarding/Onboarding.style.ts
@@ -1,9 +1,35 @@
 import { StyleSheet } from 'react-native';
+import { spacing } from '../../themes/spacing';
+import { theme } from '../../themes';
 
 export const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: theme.palette.white.dark,
+    paddingHorizontal: spacing.averageWidth,
+    paddingVertical: spacing.bigHeight,
+  },
+  headerText: {
+    fontSize: theme.typography.fontSize.subTitle,
+    color: theme.palette.black.dark,
+    textAlign: 'center',
+    fontWeight: '500',
+    opacity: 0.8,
+  },
+  bottomShape: {
+    backgroundColor: theme.palette.white.dark,
+  },
+  btn: {
+    color: theme.palette.primary.light,
+    fontSize: theme.typography.fontSize.average,
+    fontWeight: '500',
+  },
+  subTitle: {
+    paddingTop: spacing.averageHeight,
+    fontSize: theme.typography.fontSize.big,
+    color: theme.palette.black.dark,
+    textAlign: 'center',
+    fontWeight: '400',
+    opacity: 0.6,
   },
 });
--- src/screens/Onboarding/Onboarding.tsx
+++ src/screens/Onboarding/Onboarding.tsx
@@ -1,39 +1,31 @@
 import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { Image } from 'react-native';
-import Onboarding from 'react-native-onboarding-swiper';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { BottomShape } from '../../components';
 import { APP_ROUTES } from '../../constants/enums';
 import { NavigationParams } from '../../types/types';
-import React from 'react';
+import { styles } from './Onboarding.style';
 
 const OnboardingComponent = () => {
   const navigation = useNavigation<NavigationProp<NavigationParams>>();
+
   const handleNext = () => {
     navigation.navigate(APP_ROUTES.launch);
   };
 
   return (
-    <Onboarding
-      pages={[
-        {
-          backgroundColor: '#fff',
-          image: <Image source={require('../../assets/images/coinchums.png')} />,
-          title: 'Welcome',
-          subtitle: 'This is the welcome screen',
-        },
-        {
-          backgroundColor: '#fff',
-          title: 'Introduction',
-          subtitle: 'Introduce your app here',
-          image: <Image source={require('../../assets/images/coinchums.png')} />,
-        },
-      ]}
-      onSkip={handleNext}
-      onDone={handleNext}
-      showSkip={true}
-      bottomBarHighlight={false}
-      nextLabel="Next"
-      skipLabel="Skip"
-    />
+    <>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Welcome to CoinChums!</Text>
+        <Text style={styles.subTitle}>With CoinChums, Track balances effortlessly among friends or groups</Text>
+        <TouchableOpacity onPress={handleNext}>
+          <Text style={styles.btn}>Skip</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomShape}>
+        <BottomShape />
+      </View>
+    </>
   );
 };
 
