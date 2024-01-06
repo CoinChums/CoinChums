e9b76a77c1303be2a21bf2d048610e9b4b3d2e94
SurajGupta07 <bgsuraj07@gmail.com>
2024-01-07 00:46:15 +0530

    refactor: Home screen UI added

--- src/screens/Home/Home.style.ts
+++ src/screens/Home/Home.style.ts
@@ -1,9 +1,15 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../themes';
+import { spacing } from '../../themes/spacing';
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingHorizontal: spacing.smallWidth,
+  },
+  title: {
+    fontSize: theme.typography.fontSize.big,
+    fontFamily: theme.typography.fontFamily[700],
+    color: theme.palette.secondary.dark,
+    marginVertical: spacing.smallHeight,
   },
 });
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { FlatList } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { BaseLayout, GroupListTile, Header } from '../../components';
 import { expense } from '../../mocks';
 import { THome } from './types';
+import { styles } from './Home.style';
 
 const Home: React.FC<THome> = () => {
   const { t } = useTranslation();
+  const header = 'Overall, you owe ₹ 300';
 
   return (
     <BaseLayout>
       <Header title={t('home')} />
-      <FlatList data={expense.groups} renderItem={({ item }) => <GroupListTile node={item} />} />
+      <View style={styles.container}>
+        <Text style={styles.title}>{header}</Text>
+        <FlatList data={expense.groups} renderItem={({ item }) => <GroupListTile node={item} />} />
+      </View>
     </BaseLayout>
   );
 };
