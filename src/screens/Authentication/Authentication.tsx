9f4fb87933facb1b9f8e537bf57cb191dd51ba63
SurajGupta07 <bgsuraj07@gmail.com>
2024-01-09 17:12:31 +0530

    chore: updated workflow build

--- src/screens/Authentication/Authentication.tsx
+++ src/screens/Authentication/Authentication.tsx
@@ -1,3 +1,4 @@
+import { WEB_CLIENT_ID } from '@env';
 import auth from '@react-native-firebase/auth';
 import React from 'react';
 import { View } from 'react-native';
@@ -6,6 +7,12 @@ import { loginAction } from '../../redux/login/saga/login.actions';
 import { useAppDispatch } from '../../redux/reduxStore';
 import { styles } from './Authentication.style';
 import { BUTTON_TYPE } from '../../constants/enums';
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId: WEB_CLIENT_ID,
+  offlineAccess: true,
+});
 
 const Authentication = () => {
   const dispatch = useAppDispatch();
@@ -19,9 +26,29 @@ const Authentication = () => {
       console.error(err);
     }
   };
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('SIGN_IN_CANCELLED');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('IN_PROGRESS');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('PLAY_SERVICES_NOT_AVAILABLE');
+      } else {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Button type={BUTTON_TYPE.FILL} title="Login as Guest" onPress={anonymousSignIn} />
+      <GoogleSigninButton onPress={signIn} />
     </View>
   );
 };
