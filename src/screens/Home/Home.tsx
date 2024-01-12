39f2b48d8c5e7dff21d7f2ab5a682dc5eb207e45
SurajGupta07 <bgsuraj07@gmail.com>
2024-01-12 23:38:56 +0530

    chore: update workflows build cache

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { FlatList, Text, View } from 'react-native';
-import { BaseLayout, Button, ExpenseTree, GroupListTile, Header, OverlayModal } from '../../components';
+import { BaseLayout, Button, GroupListTile, Header } from '../../components';
 import { BUTTON_TYPE } from '../../constants/enums';
-import { expensTree, expense } from '../../mocks';
-import dimensions from '../../utils/dimensions';
+import { expense } from '../../mocks';
 import { APP_IMAGES } from '../../utils/imageMapper';
 import { styles } from './Home.style';
 import { THome } from './types';
@@ -12,7 +11,6 @@ import { THome } from './types';
 const Home: React.FC<THome> = () => {
   const { t } = useTranslation();
   const header = 'Overall, you owe ₹ 300';
-  const [showPopup, setShowPopup] = useState(false);
 
   return (
     <BaseLayout style={styles.home}>
@@ -24,13 +22,6 @@ const Home: React.FC<THome> = () => {
           <Button title={'Create a new group'} type={BUTTON_TYPE.OUTLINE} leftIcon={APP_IMAGES.userGroup} />
         </View>
       </View>
-      <Button type={BUTTON_TYPE.FILL} title="Popup" onPress={() => setShowPopup(true)} />
-      <OverlayModal width={dimensions.viewHeight(300)} visible={showPopup} onRequestClose={() => setShowPopup(false)}>
-        <View>
-          <Text>Modal Component Example</Text>
-        </View>
-      </OverlayModal>
-      <ExpenseTree iconSrc={APP_IMAGES.profile} title="suraj paid ₹900.00" expenseTree={expensTree} />
     </BaseLayout>
   );
 };
