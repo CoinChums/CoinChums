414bea2bdc23f6b680242e2495109ff9e0683fb6
SurajGupta07 <bgsuraj07@gmail.com>
2024-02-26 01:50:37 +0530

    chore: remove fb

--- src/screens/Authentication/Authentication.tsx
+++ src/screens/Authentication/Authentication.tsx
@@ -1,11 +1,10 @@
-import auth from '@react-native-firebase/auth';
 import React from 'react';
 import { View } from 'react-native';
+import { AuthActions } from '../../actions/auth';
 import { Button } from '../../components';
 import { BUTTON_TYPE } from '../../constants/enums';
-import { styles } from './Authentication.style';
 import { useAuth } from '../../store/useAuth/useAuth';
-import { AuthActions } from '../../actions/auth';
+import { styles } from './Authentication.style';
 
 const Authentication = () => {
   const { loginSuccess } = useAuth();
@@ -23,29 +22,9 @@ const Authentication = () => {
     }
   };
 
-  const emailPass = () => {
-    auth()
-      .createUserWithEmailAndPassword('jane.doe@example.com', 'SuperSecretPassword!')
-      .then(() => {
-        console.log('User account created & signed in!');
-      })
-      .catch(error => {
-        if (error.code === 'auth/email-already-in-use') {
-          console.log('That email address is already in use!');
-        }
-
-        if (error.code === 'auth/invalid-email') {
-          console.log('That email address is invalid!');
-        }
-
-        console.error(error);
-      });
-  };
-
   return (
     <View style={styles.container}>
       <Button type={BUTTON_TYPE.FILL} title="Login as Guest" onPress={anonymousSignIn} />
-      <Button type={BUTTON_TYPE.FILL} title="Login email pass" onPress={emailPass} />
     </View>
   );
 };
