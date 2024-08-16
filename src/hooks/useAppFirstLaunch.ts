757417fb0a51bdb65966ff813f6f3a52daa42f13
SurajGupta07 <bgsuraj07@gmail.com>
2024-08-16 19:07:36 +0530

    refactor: Move to storage to services

--- src/hooks/useAppFirstLaunch.ts
+++ src/hooks/useAppFirstLaunch.ts
@@ -1,6 +1,6 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
 import { ASYNC_STORAGE } from '../constants/enums';
+import { getAsyncItem, setAsyncItem } from '../services/storage.service';
 
 export const useAppFirstLaunch = () => {
   const [isFirstLaunch, setIsFirstLaunch] = useState<boolean | null>(null);
@@ -8,9 +8,9 @@ export const useAppFirstLaunch = () => {
   useEffect(() => {
     const checkFirstLaunch = async () => {
       try {
-        const value = await AsyncStorage.getItem(ASYNC_STORAGE.IS_FIRSTLAUNCH);
+        const value = await getAsyncItem(ASYNC_STORAGE.IS_FIRSTLAUNCH);
         if (value === null) {
-          await AsyncStorage.setItem(ASYNC_STORAGE.IS_FIRSTLAUNCH, 'true');
+          await setAsyncItem(ASYNC_STORAGE.IS_FIRSTLAUNCH, 'true');
           setIsFirstLaunch(true);
         } else {
           setIsFirstLaunch(false);
